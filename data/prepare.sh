c98c2c5e87b80547980dbe225f9d4347a89d1cbb
Ilya Zverev <ilya@zverev.info>
2026-03-29 15:16:19 +0300

    Prepare for deployment

--- data/prepare.sh
+++ data/prepare.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 set -e -u
+SOURCE=estonia-latest.osm.pbf
 BBOX=24.5185,59.322118,25.014945,59.533062
+TMP=/tmp
+HERE=$(dirname "$0")
 FORMAT=pmtiles
-TIPPECANOE=../../tippecanoe/tippecanoe
+TIPPECANOE=${TIPPECANOE:-../../tippecanoe/tippecanoe}
+OSMIUM=${OSMIUM:-osmium}
+UV=${UV:-uv}
 
-if [ ! -e estonia-latest.osm.pbf ]; then
-  wget https://download.geofabrik.de/europe/estonia-latest.osm.pbf -O estonia-latest.osm.pbf
+if [ ! -e $SOURCE ]; then
+  wget https://download.geofabrik.de/europe/$SOURCE -O $SOURCE
 fi
 
-osmium extract -b $BBOX -O estonia-latest.osm.pbf -o tallinn.osm.pbf
-osmium tags-filter -O tallinn.osm.pbf w/highway -o highways.osm.pbf
-osmium export highways.osm.pbf -O -o cycleways.jsonl -f jsonseq -c osmium-export-config.json
-rm tallinn.osm.pbf
-rm highways.osm.pbf
-uv run process.py cycleways.jsonl -o cycleways2.jsonl
-uv run trim_measure.py cycleways2.jsonl -p tallinn.json -l -o toelised-rattateed.jsonl
-rm cycleways.jsonl
-rm cycleways2.jsonl
+$OSMIUM extract -b $BBOX -O $SOURCE -o $TMP/tallinn.osm.pbf
+$OSMIUM tags-filter -O $TMP/tallinn.osm.pbf w/highway -o $TMP/highways.osm.pbf
+$OSMIUM export $TMP/highways.osm.pbf -O -o $TMP/cycleways.jsonl -f jsonseq -c osmium-export-config.json
+rm $TMP/tallinn.osm.pbf
+rm $TMP/highways.osm.pbf
+
+if $UV -V; then
+  $UV run process.py $TMP/cycleways.jsonl -o $TMP/cycleways2.jsonl
+  $UV run trim_measure.py $TMP/cycleways2.jsonl -p tallinn.json -l -o toelised-rattateed.jsonl
+else
+  python3 process.py $TMP/cycleways.jsonl -o toelised-rattateed.jsonl
+fi
+rm $TMP/cycleways.jsonl
+rm $TMP/cycleways2.jsonl
+
 rm -f toelised-rattateed.$FORMAT
 $TIPPECANOE -z 13 -o toelised-rattateed.$FORMAT \
   -l rattateed --use-attribute-for-id=way_id toelised-rattateed.jsonl
